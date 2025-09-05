6c7c58e4622ff21e7e4353dfd20163ca91b37ad0
Adil-Ir <dev.adil@irsolutions.tech>
2025-09-05 23:51:06 +0500

    dirty commit

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,12 +3,12 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-import prettier from "eslint-plugin-prettier";
-import eslintConfigPrettier from "eslint-config-prettier";
+// @ts-expect-error - no types provided
+import pluginPrettier from "eslint-plugin-prettier";
+import * as configPrettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
- 
   {
     files: ["**/*.{js,mjs,cjs}"],
     ...js.configs.recommended,
@@ -17,27 +17,23 @@ export default defineConfig([
     },
   },
 
-  
   ...tseslint.configs.recommended,
 
-  
   {
-    files: ["**/*.{jsx,tsx}"],
+    files: ["**/*.{jsx,tsx,ts,js}"],
     ...pluginReact.configs.flat.recommended,
     plugins: {
       react: pluginReact,
       "react-hooks": reactHooks,
-      prettier,
+      prettier: pluginPrettier,
     },
     rules: {
       ...pluginReact.configs.flat.recommended.rules,
       ...reactHooks.configs.recommended.rules,
 
-    
       "prettier/prettier": "error",
-
-      "react/react-in-jsx-scope": "off", 
-      "react/prop-types": "off", 
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
       "@typescript-eslint/no-unused-vars": ["warn"],
     },
     settings: {
@@ -47,6 +43,7 @@ export default defineConfig([
     },
   },
 
-
-  eslintConfigPrettier,
+  {
+    rules: configPrettier.rules,
+  },
 ]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 const App = () => {
-  console.log('App rendered');
-  djdfj kjer 
   return (
-    <div className="py-20 text-center w-full">
-      <h1 className="heading-lg bg-amber-200">
-        The quick brown fox jumps over the lazy dog.
-      </h1>
+    <div className="py-20   text-center   w-full">
+      <h1 className="heading-lg bg-amber-200">The quick brown fox jumps over the lazy dog.</h1>
       <p className="paragraph-lg mt-5">
-        This pangram contains every letter of the English alphabet at least
-        once. Here is another line to check how the spacing looks. Font scaling
-        should be consistent across different devices. Responsive design helps
-        maintain readability everywhere.
+        {" "}
+        This pangram contains every letter of the English alphabet at least once Here is another
+        line to check how the spacing looks Font scaling should be consistent across different
+        devices Responsive design helps maintain readability everywhere
       </p>
     </div>
   );
