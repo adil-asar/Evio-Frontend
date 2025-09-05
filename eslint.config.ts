9ffba0907294cf6809f924c8b5323c03f69e7b39
Adil-Ir <dev.adil@irsolutions.tech>
2025-09-05 23:23:57 +0500

    done

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,10 +2,51 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+ 
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ...js.configs.recommended,
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+
+  
+  ...tseslint.configs.recommended,
+
+  
+  {
+    files: ["**/*.{jsx,tsx}"],
+    ...pluginReact.configs.flat.recommended,
+    plugins: {
+      react: pluginReact,
+      "react-hooks": reactHooks,
+      prettier,
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+    
+      "prettier/prettier": "error",
+
+      "react/react-in-jsx-scope": "off", 
+      "react/prop-types": "off", 
+      "@typescript-eslint/no-unused-vars": ["warn"],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+
+
+  eslintConfigPrettier,
 ]);
