0f0aed9511247ab2c58d852c36af80eec2e188ed
Adil-Ir <dev.adil@irsolutions.tech>
2025-09-10 23:15:29 +0500

    ddfjdf

--- src/components/common/Tagline.tsx
+++ src/components/common/Tagline.tsx
@@ -1,5 +1,9 @@
-// Tagline.jsx
-const Tagline = ({ text, className = "" }) => {
+type TaglineProps = {
+  text: string;
+  className?: string;
+};
+
+const Tagline = ({ text, className = "" }: TaglineProps) => {
   return (
     <div
       className={`paragraph-sm mb-8 max-w-[260px] mx-auto text-center bg-accent rounded-full py-1.5 border-glow ${className}`}
