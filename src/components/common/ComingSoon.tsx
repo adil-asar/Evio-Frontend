cc14cbd3f99443cb2e4517906ab6bff9ccfdc0ee
Adil-Ir <dev.adil@irsolutions.tech>
2025-09-14 20:40:15 +0500

    done

--- src/components/common/ComingSoon.tsx
+++ src/components/common/ComingSoon.tsx
@@ -1,23 +1,19 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const ComingSoon = () => {
   return (
-    <div className="containerize py-32">
-      <div className="text-center heading-lg">
+    <div className="containerize py-20">
+      <div className="text-center ">
         {/* Heading */}
-        <h1 className="text-5xl font-bold mb-4">🚀 Coming Soon</h1>
+        <h1 className="heading-lg text-gradient"> Coming Soon</h1>
 
         {/* Subtitle */}
-        <p className="paragraph-lg mb-8">
+        <p className="paragraph-lg mt-5 mb-10">
           We’re working hard to bring you something amazing. Stay tuned — it won’t take long!
         </p>
 
         {/* Button */}
-        <Link
-          to="/"
-          className="inline-block px-6 py-3 rounded-xl bg-blue-600 hover:bg-blue-500 transition-all font-medium shadow-lg"
-        >
+        <Link to="/" className="btn mt-5">
           Back to Home
         </Link>
       </div>
