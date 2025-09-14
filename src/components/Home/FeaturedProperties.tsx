e364a90408a99bc2fec675650b8588e88e44d305
Adil-Ir <dev.adil@irsolutions.tech>
2025-09-14 18:57:45 +0500

    done

--- src/components/Home/FeaturedProperties.tsx
+++ src/components/Home/FeaturedProperties.tsx
@@ -1,5 +1,15 @@
 const FeaturedProperties = () => {
-  return <div>FeaturedProperties</div>;
+  return (
+    <div>
+      <div>
+        <h1>Explore Featured Properties</h1>
+        <h2>
+          Discover handpicked homes and investment properties in prime locations. From luxury
+          apartments to family houses and commercial spaces, Evio has something for everyone.
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedProperties;
